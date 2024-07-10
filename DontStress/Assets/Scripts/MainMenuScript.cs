470f989fe830cd74122139e6e68800e61d7b2ae5
Arthur <arthurbr1112004@gmail.com>
2024-07-10 15:18:31 +0100

    Added scene loading and unloading

--- DontStress/Assets/Scripts/MainMenuScript.cs
+++ DontStress/Assets/Scripts/MainMenuScript.cs
@@ -2,113 +2,94 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+// public class MainMenuScript : MonoBehaviour
+// {
+//     public void PlayGame()
+//     {
+//         SceneManager.LoadSceneAsync("Game");
+//     }
+// }
 
 public class MainMenuScript : MonoBehaviour
 {
-    public void PlayGame()
+    public GameObject mainCamera;
+    public GameObject eventSystems; 
+    private bool gameSceneLoaded = false;
+
+    private void Awake()
     {
-        SceneManager.LoadSceneAsync("Game");
+        ManageEventSystems();
+        ManageAudioListeners();
     }
 
+    public void PlayGame()
+    {
+        // eventSystems.SetActive(false);
+        // mainCamera.GetComponent<AudioListener>().enabled = false;
+        // mainCamera.SetActive(false);
+        // SceneManager.LoadSceneAsync("Game", LoadSceneMode.Additive);
+        SceneManager.LoadSceneAsync("Game");
+        gameSceneLoaded = true;
+    }
+    private void ResumeGame()
+    {
+        GlobalVariables.Paused = false;
+        Time.timeScale = 1f;
+    }
+    private void PauseGame()
+    {
+        GlobalVariables.Paused = false;
+        Time.timeScale = 0f;
+    }
+
+     public void ReturnToMenu()
+    {
+        if (gameSceneLoaded)
+        {
+            SceneManager.UnloadSceneAsync("Game");
+            gameSceneLoaded = false;
+        }
+        eventSystems.SetActive(true);
+        mainCamera.GetComponent<AudioListener>().enabled = true;
+        mainCamera.SetActive(true);
+    }
+
+    #region Management
+    private void ManageEventSystems()
+    {
+        EventSystem[] eventSystems = FindObjectsOfType<EventSystem>();
+
+        // If more than one EventSystem is found, disable all but one
+        if (eventSystems.Length > 1)
+        {
+            for (int i = 1; i < eventSystems.Length; i++)
+            {
+                eventSystems[i].gameObject.SetActive(false);
+                Debug.LogWarning("Disabled extra EventSystem on: " + eventSystems[i].gameObject.name);
+            }
+        }
+    }
+
+    private void ManageAudioListeners()
+    {
+        AudioListener[] audioListeners = FindObjectsOfType<AudioListener>();
+
+        // If more than one AudioListener is found, disable all but one
+        if (audioListeners.Length > 1)
+        {
+            for (int i = 1; i < audioListeners.Length; i++)
+            {
+                audioListeners[i].gameObject.SetActive(false);
+                Debug.LogWarning("Disabled extra AudioListener on: " + audioListeners[i].gameObject.name);
+            }
+        }
+    }
+    #endregion
     public void QuitGame()
     {
         Application.Quit();
     }
 }
-
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-// using UnityEngine.SceneManagement;
-// using UnityEngine.EventSystems;
-
-// public class MainMenuScript : MonoBehaviour
-// {
-//     public GameObject mainCamera;
-//     public GameObject eventSystems; 
-//     private bool gameSceneLoaded = false;
-
-//     private void Awake()
-//     {
-//         ManageEventSystems();
-//         ManageAudioListeners();
-//         if (SceneManager.GetSceneByName("Game").isLoaded)
-//         {   
-//             PauseGame();
-//             Debug.Log("Pause Game");
-//             // SceneManager.UnloadSceneAsync("Game");
-//             // Debug.Log("UnloadingSceneGame");
-//         }
-//     }
-
-//     public void PlayGame()
-//     {
-//         // eventSystems.SetActive(false);
-//         // mainCamera.GetComponent<AudioListener>().enabled = false;
-//         // mainCamera.SetActive(false);
-//         ResumeGame();
-//         SceneManager.UnloadSceneAsync("MainMenu");
-//         Debug.Log("Resume Game");
-//         // SceneManager.LoadSceneAsync("Game", LoadSceneMode.Additive);
-//         // Debug.Log("LoadingGame");
-//         gameSceneLoaded = true;
-//     }
-//     private void ResumeGame()
-//     {
-//         GlobalVariables.Paused = false;
-//         Time.timeScale = 1f;
-//     }
-//     private void PauseGame()
-//     {
-//         GlobalVariables.Paused = false;
-//         Time.timeScale = 0f;
-//     }
-//     public void QuitGame()
-//     {
-//         Application.Quit();
-//     }
-
-//      public void ReturnToMenu()
-//     {
-//         if (gameSceneLoaded)
-//         {
-//             SceneManager.UnloadSceneAsync("Game");
-//             gameSceneLoaded = false;
-//         }
-//         eventSystems.SetActive(true);
-//         mainCamera.GetComponent<AudioListener>().enabled = true;
-//         mainCamera.SetActive(true);
-//     }
-
-//     private void ManageEventSystems()
-//     {
-//         EventSystem[] eventSystems = FindObjectsOfType<EventSystem>();
-
-//         // If more than one EventSystem is found, disable all but one
-//         if (eventSystems.Length > 1)
-//         {
-//             for (int i = 1; i < eventSystems.Length; i++)
-//             {
-//                 eventSystems[i].gameObject.SetActive(false);
-//                 Debug.LogWarning("Disabled extra EventSystem on: " + eventSystems[i].gameObject.name);
-//             }
-//         }
-//     }
-
-//     private void ManageAudioListeners()
-//     {
-//         AudioListener[] audioListeners = FindObjectsOfType<AudioListener>();
-
-//         // If more than one AudioListener is found, disable all but one
-//         if (audioListeners.Length > 1)
-//         {
-//             for (int i = 1; i < audioListeners.Length; i++)
-//             {
-//                 audioListeners[i].gameObject.SetActive(false);
-//                 Debug.LogWarning("Disabled extra AudioListener on: " + audioListeners[i].gameObject.name);
-//             }
-//         }
-//     }
-
-// }
 

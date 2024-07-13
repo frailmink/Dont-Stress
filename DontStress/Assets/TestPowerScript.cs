963a7a19e00d596ac1cde4785862d4d5d011d414
sc222as-leeds <sc222as@leeds.ac.uk>
2024-07-13 15:36:27 +0100

    Made the Power basic system (now its easy to create new powers)

---
+++ DontStress/Assets/TestPowerScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPowerScript : PowerScript
+{
+    public override void Run()
+    {
+        Debug.Log("ran");
+    }
+}
